6e5c3d62379fbc291eefe05faf41936c0047b0b7
Abhinai <Abhinai13rules@gmail.com>
2019-04-28 19:33:39 -0400

    11th commit- Feature Test for About Page

--- tests/Feature/LoginPageTest.php
+++ tests/Feature/LoginPageTest.php
@@ -6,7 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class LoginPageTestest extends TestCase
+class LoginPageTest extends TestCase
 {
     /**
      * A basic feature test example.

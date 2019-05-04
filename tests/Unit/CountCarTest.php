a90647cfa09f14b96401a13f1dde8c13e390b68c
Abhinai <Abhinai13rules@gmail.com>
2019-05-04 16:53:24 -0400

    20th commit- Feature Test for Counting car seed

---
+++ tests/Unit/CountCarTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CountCarTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $carcount = \App\Car::count();
+        $isSeeded = false;
+        if($carcount >= "50" ){
+            $isSeeded = true;
+        }
+        $this->assertTrue($isSeeded);
+
+
+    }
+}
